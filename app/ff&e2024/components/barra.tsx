41eb4385358f363f66142d6c2114c9b417d35ceb
ermeneses <132471398+ermeneses@users.noreply.github.com>
2025-02-14 16:26:04 -0500

    años

--- app/ff&e2024/components/barra.tsx
+++ app/ff&e2024/components/barra.tsx
@@ -8,49 +8,52 @@ interface Props {
   catalogo: number;
   aprobacion: number;
   proveedor: number;
+  año?: number;
 }
 const Barra = (props: Props) => {
   return (
-    <div className="w-full flex flex-col items-start justify-start gap-2 ">
-      <div className="w-full md:w-3/5 xl:w-3/5 lg:w-3/5 flex flex-row items-center justify-between flex-wrap gap-2 self-center ">
-        <div className="flex w-full sm:w-auto md:w-auto flex-col items-start justify-center flex-none ">
+    <div className="w-full flex flex-col items-start justify-start gap-2">
+      <div className="w-full md:w-3/5 xl:w-3/5 lg:w-3/5 flex flex-row items-center justify-between flex-wrap gap-2 self-center">
+        <div className="flex sm:w-auto md:w-auto flex-col items-start justify-center flex-none w-full">
           <h1 className="leading-none font-bold uppercase">{props.destino}</h1>
-          <h4 className="leading-none font-light">FF&amp;E 2023</h4>
+          <h4 className="leading-none font-light">FF&amp;E {props.año ?? ""}</h4>
         </div>
-        <div className="flex flex-col items-center justify-center self-stretch">
-          <h1 className="leading-none text-xl">{props.actividades}</h1>
-          <h4 className="leading-none font-light">Actividades</h4>
-        </div>
-        <div className="flex flex-col items-center justify-center self-stretch">
-          <h1 className="leading-none text-xl">{props.avance.toFixed(1)}%</h1>
-          <h4 className="leading-none font-light">Avance Global</h4>
-        </div>
-        <div className="flex flex-col items-start justify-center self-stretch text-xs uppercase">
-          <div className="flex items-center justify-start gap-1">
-            <div className="w-2 h-2 rounded-sm bg-green-400"></div>
-            <h4 className="leading-none font-light">{props.finalizadas}- Finalizadas</h4>
+        <div className="flex items-center justify-start gap-10">
+          <div className="flex flex-col items-center justify-center self-stretch">
+            <h1 className="leading-none text-xl">{props.actividades}</h1>
+            <h4 className="leading-none font-light">Actividades</h4>
           </div>
-          <div className="flex items-center justify-start gap-1">
-            <div className="w-2 h-2 rounded-sm bg-red-400"></div>
-            <h4 className="leading-none font-light">{props.cotizacion}- En Cotizacion</h4>
+          <div className="flex flex-col items-center justify-center self-stretch">
+            <h1 className="leading-none text-xl">{props.avance.toFixed(1)}%</h1>
+            <h4 className="leading-none font-light">Avance Global</h4>
           </div>
-          <div className="flex items-center justify-start gap-1">
-            <div className="w-2 h-2 rounded-sm bg-teal-400"></div>
-            <h4 className="leading-none font-light">{props.ejecucion}- En Ejecución</h4>
+          <div className="flex flex-col items-start justify-center self-stretch text-xs uppercase">
+            <div className="flex items-center justify-start gap-1">
+              <div className="w-2 h-2 rounded-sm bg-green-400"></div>
+              <h4 className="leading-none font-light">{props.finalizadas}- Finalizadas</h4>
+            </div>
+            <div className="flex items-center justify-start gap-1">
+              <div className="w-2 h-2 rounded-sm bg-red-400"></div>
+              <h4 className="leading-none font-light">{props.cotizacion}- En Cotizacion</h4>
+            </div>
+            <div className="flex items-center justify-start gap-1">
+              <div className="w-2 h-2 rounded-sm bg-teal-400"></div>
+              <h4 className="leading-none font-light">{props.ejecucion}- En Ejecución</h4>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-start justify-center self-stretch text-xs uppercase">
-          <div className="flex items-center justify-start gap-1">
-            <div className="w-2 h-2 rounded-sm bg-orange-400"></div>
-            <h4 className="leading-none font-light">{props.catalogo}- En Catálogo C.</h4>
-          </div>
-          <div className="flex items-center justify-start gap-1">
-            <div className="w-2 h-2 rounded-sm bg-yellow-400"></div>
-            <h4 className="leading-none font-light">{props.aprobacion}- Aprobación</h4>
-          </div>
-          <div className="flex items-center justify-start gap-1">
-            <div className="w-2 h-2 rounded-sm bg-blue-400"></div>
-            <h4 className="leading-none font-light">{props.proveedor}- P. Proveedor</h4>
+          <div className="flex flex-col items-start justify-center self-stretch text-xs uppercase">
+            <div className="flex items-center justify-start gap-1">
+              <div className="w-2 h-2 rounded-sm bg-orange-400"></div>
+              <h4 className="leading-none font-light">{props.catalogo}- En Catálogo C.</h4>
+            </div>
+            <div className="flex items-center justify-start gap-1">
+              <div className="w-2 h-2 rounded-sm bg-yellow-400"></div>
+              <h4 className="leading-none font-light">{props.aprobacion}- Aprobación</h4>
+            </div>
+            <div className="flex items-center justify-start gap-1">
+              <div className="w-2 h-2 rounded-sm bg-blue-400"></div>
+              <h4 className="leading-none font-light">{props.proveedor}- P. Proveedor</h4>
+            </div>
           </div>
         </div>
       </div>
